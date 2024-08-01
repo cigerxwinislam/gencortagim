2572de3ef6b28c390a41ee8902c1eb1f18df206a
Cigerxwin İslam <islamtunc748@gmail.com>
2024-08-01 17:00:09 +0300

    ser xere nu

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/genc-ortagim");
+    await mongoose.connect(process.env.MONGODB);
     console.log(" MongoDB ye bağlandı");
   } catch (error) {
     console.log("Bağlanamadı: ", error);
